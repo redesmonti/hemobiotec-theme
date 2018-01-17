2cf85c99968506afd0ad76a964050ad6b4d2aa32
mnaviaz <michelle.navia3@gmail.com>
2018-01-17 18:12:28 -0300

    estilos noticias

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <section>
-  	<div class="container">
+  	<div class="container noticia">
   		<div class="contenedor-entradas">
         <div class="col-md-8 fondo-blanco">
           <div class="titulo">
@@ -11,15 +11,16 @@
             <?php  if ( has_post_thumbnail() ) { the_post_thumbnail('medium', array('class' => 'img-responsive')); }?>
             <p><?php the_content(); ?></p>
           </div>
+          <div class="col-xs-12 navegavion">
+          <?php previous_post_link('%link', '<i class="fa fa-arrow-left"></i> Atras '); ?><!--hacia atras-->
+          <?php next_post_link('%link', ' Siguiente <i class="fa fa-arrow-right"></i>'); ?><!--hacia adelante-->
+        </div>
         </div>
         <div class="col-md-4">
           <?php dynamic_sidebar( 'sidebar' ); ?>
         </div>
   		</div>
-      <div class="col-xs-6">
-          <?php previous_post_link('%link', '<i class="fa fa-arrow-left"></i> Atras '); ?><!--hacia atras-->
-          <?php next_post_link('%link', ' Siguiente <i class="fa fa-arrow-right"></i>'); ?><!--hacia adelante-->
-        </div>
+      
       <?php endif; ?>
   	</div>
 </section>
