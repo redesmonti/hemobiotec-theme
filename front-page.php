cd19a85b9417c123625e248a6d30899479cc59f2
CarlosMellaN <carlosmellaneira@gmail.com>
2018-01-19 09:32:34 -0300

    slider plugin
    x:

--- front-page.php
+++ front-page.php
@@ -1,34 +1,8 @@
 <?php get_header(); ?>
-	<div id="carousel-example-generic" class="carousel slide slider-principal" data-ride="carousel">
-	  <!-- Indicators -->
-	  <ol class="carousel-indicators">
-	    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-	    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-	    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-	  </ol>
-	  	  <!-- Wrapper for slides -->
-	  <div class="carousel-inner" role="listbox">
-	    <div class="item active">
-	      <img src="<?php echo get_template_directory_uri(); ?>/assets/slider1.png" alt="...">
-	      <div class="carousel-caption">
-	      	<div class="titulo-principal">
-	      		<h1>Título Principal</h1>
-	      		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
-	      		<button>Ver más</button>
-	      	</div>
-	      </div>
-	    </div>
-	  </div>
-	    <!-- Controls -->
-	  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-	    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-	    <span class="sr-only">Previous</span>
-	  </a>
-	  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-	    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-	    <span class="sr-only">Next</span>
-	  </a>
-	</div>	
+	<section name="slider">
+		<?php echo do_shortcode('[metaslider id="44"]'); ?>
+
+	</section>
 
 	<!--- botones laboratorio y equipo -->
 	<div class="contenedor-botones container-fluid">
